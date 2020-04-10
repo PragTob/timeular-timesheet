32204a1a9a07bbd83eaf516880dadcf4af282fb9
Tobias Pfeiffer <pragtob@gmail.com>
2020-04-10 15:20:24 +0200

    additional totalTime testing

--- src/lib/report.test.js
+++ src/lib/report.test.js
@@ -29,6 +29,36 @@ describe("Report", () => {
       expect(result.hourFormat).toEqual("1:55:33h");
       expect(result.floatFormat).toEqual("1.93h");
 
-    })
+    });
+
+    test("time of two activities", () => {
+      const timeEntries = [
+        {
+          acivity: {
+            name: "Test"
+          },
+          duration: {
+            startedAt: "2020-03-10T10:07:30.000",
+            stoppedAt: "2020-03-10T12:03:03.192"
+          }
+        },
+        {
+          acivity: {
+            name: "Test2"
+          },
+          duration: {
+            startedAt: "2020-02-21T23:07:30.000",
+            stoppedAt: "2020-02-21T23:10:30.000"
+          }
+        }
+      ]
+
+      const result = report(timeEntries).totalDuration;
+
+      expect(result.milliseconds).toEqual(7113192);
+      expect(result.hourFormat).toEqual("1:58:33h");
+      expect(result.floatFormat).toEqual("1.98h");
+
+    });
   });
 });
